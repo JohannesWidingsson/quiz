4722cd7c74db34bd7973358491b7e2c6c840c229
JohannesWidingsson <73167271+JohannesWidingsson@users.noreply.github.com>
2021-03-05 11:31:55 +0100

    Initial commit

---
+++ Main.java
@@ -0,0 +1,30 @@
+package quiz;
+
+import java.io.IOException;
+import java.lang.String;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.stream.Stream;
+
+//
+
+public class Main {
+
+    public static void main(String[] args) throws IOException, Exception {
+
+    //Quiz.startMenu();
+
+        //testList.add(new Quiz("fr´ga","kalle","kalle12","kalle4"));
+
+       // System.out.println(testList);
+
+        //Quiz.startMenu();
+
+        Quiz.questionsFile();
+
+
+    }
+
+}
---
+++ Person.java
@@ -0,0 +1,17 @@
+package quiz;
+
+
+import java.io.Serializable;
+
+abstract public class Person implements Serializable {
+
+    String name;
+    int age;
+    String eMail;
+
+    public Person(String name, int age, String eMail) {
+        this.name = name;
+        this.age = age;
+        this.eMail = eMail;
+    }
+}
